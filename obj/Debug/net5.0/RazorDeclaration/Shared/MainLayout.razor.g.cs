12670aa2d8a20d87610733cbdca581442a1ec348
LadyMisfortunate <80325540+LadyMisfortunate@users.noreply.github.com>
2022-02-03 23:32:58 +0100

    first comit

--- obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace BlazorApp.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "E:\Desktop\Angular-Blazor\BlazorApp\_Imports.razor"
+#line 1 "e:\Desktop\Angular-Blazor\Angular in Blazor\BlazorApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "E:\Desktop\Angular-Blazor\BlazorApp\_Imports.razor"
+#line 2 "e:\Desktop\Angular-Blazor\Angular in Blazor\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "E:\Desktop\Angular-Blazor\BlazorApp\_Imports.razor"
+#line 3 "e:\Desktop\Angular-Blazor\Angular in Blazor\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "E:\Desktop\Angular-Blazor\BlazorApp\_Imports.razor"
+#line 4 "e:\Desktop\Angular-Blazor\Angular in Blazor\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "E:\Desktop\Angular-Blazor\BlazorApp\_Imports.razor"
+#line 5 "e:\Desktop\Angular-Blazor\Angular in Blazor\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "E:\Desktop\Angular-Blazor\BlazorApp\_Imports.razor"
+#line 6 "e:\Desktop\Angular-Blazor\Angular in Blazor\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "E:\Desktop\Angular-Blazor\BlazorApp\_Imports.razor"
+#line 7 "e:\Desktop\Angular-Blazor\Angular in Blazor\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "E:\Desktop\Angular-Blazor\BlazorApp\_Imports.razor"
+#line 8 "e:\Desktop\Angular-Blazor\Angular in Blazor\BlazorApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "E:\Desktop\Angular-Blazor\BlazorApp\_Imports.razor"
+#line 9 "e:\Desktop\Angular-Blazor\Angular in Blazor\BlazorApp\_Imports.razor"
 using BlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "E:\Desktop\Angular-Blazor\BlazorApp\_Imports.razor"
+#line 10 "e:\Desktop\Angular-Blazor\Angular in Blazor\BlazorApp\_Imports.razor"
 using BlazorApp.Shared;
 
 #line default
